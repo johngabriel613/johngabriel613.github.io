38f01d7428cc55f5eb5df0ec98b1ce074770efd9
John Paderog <91768994+johngabriel613@users.noreply.github.com>
2022-04-21 12:54:20 +0800

    update

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const OpenMenu = document.querySelector('.open-menu')
 const menuBar = document.querySelectorAll('.open-menu span')
 const Nav = document.querySelector('.navlist')
 
+
 console.log(menuBar)
 
 OpenMenu.addEventListener('click', () => {
@@ -22,3 +23,51 @@ darkbtn.addEventListener('click', () => [
     sunbtn.classList.toggle('on')
 
 ])
+
+
+const NavBar = document.querySelector('.navbar')
+window.addEventListener('scroll', () => {
+    if(scrollY > 20){
+        NavBar.classList.add('sticky')
+    } else {
+        NavBar.classList.remove('sticky')
+    }
+})
+
+const NavItem = document.querySelectorAll('.navlist a')
+
+NavItem[0].addEventListener('click', () => {
+    Nav.classList.remove('open')
+    menuBar[0].classList.remove('open')
+    menuBar[1].classList.remove('open')
+    menuBar[2].classList.remove('open')
+})
+NavItem[1].addEventListener('click', () => {
+    Nav.classList.remove('open')
+    menuBar[0].classList.remove('open')
+    menuBar[1].classList.remove('open')
+    menuBar[2].classList.remove('open')
+})
+NavItem[2].addEventListener('click', () => {
+    Nav.classList.remove('open')
+    menuBar[0].classList.remove('open')
+    menuBar[1].classList.remove('open')
+    menuBar[2].classList.remove('open')
+})
+NavItem[3].addEventListener('click', () => {
+    Nav.classList.remove('open')
+    menuBar[0].classList.remove('open')
+    menuBar[1].classList.remove('open')
+    menuBar[2].classList.remove('open')
+})
+
+
+const Scroll = document.querySelector('.scroll-up')
+window.addEventListener('scroll', () => {
+    if(scrollY > 20){
+        Scroll.classList.add('active')
+    } else {
+        Scroll.classList.remove('active')
+    }
+})
+
